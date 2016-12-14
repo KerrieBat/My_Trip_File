f46b51dd516952a936c0e334a108b9d00733b38a
Kerrie Batrouney <batrouney@gmail.com>
2016-12-14 14:22:11 +1100

    make menu structure and render all pages

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('views', './views')
 // app.set('stylesheets', './stylesheets')
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
+app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 const MongoClient = require('mongodb').MongoClient
 // connect to database
@@ -30,15 +31,17 @@ console.log('may node be with you')
 // })
 // // Note: request and response are usually written as req and res respectively.
 //   console.log(__dirname);
+//
+// app.post('/quotes', (req, res) => {
+//   db.collection('quotes').save(req.body, (err, result) => {
+//     if (err) return console.log(err)
+//
+//     console.log('saved to database')
+//     res.redirect('/')
+//   })
+// })
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
+// Header nav bar
 //home page
 app.get('/', (req, res) => {
 
@@ -51,33 +54,89 @@ app.get('/about', function(req, res) {
     res.render('pages/about')
 })
 
-// edit trip page
-app.get('/edit', function(req, res) {
-  db.collection('quotes').find().toArray(function(err, results) {
-  if (err) return console.log(err)
-  var trips = [
-    { name: 'Europe 2017', days: 12 },
-            { name: 'USA', days: 15 },
-            { name: 'England', days: 10 }
-        ]
-  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-
-    res.render('pages/edit_trip', {
-        quotes: results,
-        trips: trips,
-        tagline: tagline
-    })
-  })
-})
-
 // login and signup page
 app.get('/login', function(req, res) {
     res.render('pages/signin')
 })
 
+app.post('/signup', (req, res) => {
+  db.collection('users').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
+    console.log('logged in')
+    res.redirect('pages/mytrips')
+  })
+})
+
+// new trip page
+app.get('/new', function(req, res) {
+    res.render('pages/newTrip')
+})
+
+// // edit trips page
+// app.get('/myTrips', function(req, res) {
+//   db.collection('quotes').find().toArray(function(err, results) {
+//   if (err) return console.log(err)
+//   var trips = [
+//     { name: 'Europe 2017', days: 12 },
+//             { name: 'USA', days: 15 },
+//             { name: 'England', days: 10 }
+//         ]
+//   var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+//
+//
+//     res.render('pages/edit_trip', {
+//         quotes: results,
+//         trips: trips,
+//         tagline: tagline
+//     })
+//   })
+// })
+//
+
+// view trips page
+app.get('/myTrips', function(req, res) {
+    res.render('pages/myTrips')
+})
+
+//myTrips nav bar
 // view itinary page
 app.get('/itinary', function(req, res) {
     res.render('pages/itinary')
+})
+app.get('/share', function(req, res) {
+    res.render('pages/shareTrip')
+})
+app.get('/edit', function(req, res) {
+    res.render('pages/editTrip')
+})
+
+//edit trip and itinary nav bar
+app.get('/flights', function(req, res) {
+    res.render('pages/flights')
+})
+app.get('/accom', function(req, res) {
+    res.render('pages/accom')
+})
+app.get('/land', function(req, res) {
+    res.render('pages/car')
+})
+app.get('/activities', function(req, res) {
+    res.render('pages/activities')
+})
+app.get('/references', function(req, res) {
+    res.render('pages/references')
+})
+app.get('/insurance', function(req, res) {
+    res.render('pages/insurance')
+})
+app.get('/packing', function(req, res) {
+    res.render('pages/packingList')
+})
+app.get('/share', function(req, res) {
+    res.render('pages/shareTrip')
+})
+app.get('/print', function(req, res) {
+    //make it print
+    res.redirect('pages/editTrip')
 })
